9e6a7091a01bbe817f9f9ab55118e55e50239ee2
rb200117 <50101911+rb200117@users.noreply.github.com>
2021-11-11 18:31:49 +0530

    Settings Change

--- practice1/settings.py
+++ practice1/settings.py
@@ -25,6 +25,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
